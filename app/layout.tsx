b6f7ab1d0cbc9cc0b41053e064e0a7b6e9a70d17
[dyad] <igormacedo.notebook@gmail.com>
2026-05-11 15:27:44 -0300

    [dyad] (2 files changed)

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
     <html lang="pt-BR">
 <head>
   <meta charSet="utf-8" />
-  <meta name="apple-mobile-web-app-capable" content="yes" />
+  <meta name="mobile-web-app-capable" content="yes" />
   <meta name="apple-mobile-web-app-status-bar-style" content="default" />
   <meta name="apple-mobile-web-app-title" content="Impulsionaí" />
   <link rel="apple-touch-icon" href="/apple-icon.png" />
