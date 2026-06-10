c61466a33e0863063721815b59abb1b338b0a323
impulsionaimarketing <igormacedo.notebook@gmail.com>
2026-06-09 23:28:39 -0300

    Update route.ts

--- app/api/upload-video/token/route.ts
+++ app/api/upload-video/token/route.ts
@@ -1,4 +1,4 @@
-import { handleUpload, type HandleUploadBody } from '@vercel/blob/next'
+import { handleUpload, type HandleUploadBody } from '@vercel/blob/client'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
